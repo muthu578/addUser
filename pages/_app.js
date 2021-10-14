8b822ca9b0421d8667e8af383dcecb181816c8ee
muthu578 <muthummk92@gmail.com>
2021-10-14 11:11:21 +0530

    Removed code

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,32 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import 'styles/globals.css';
-import { Nav, Alert } from 'components';
+import "styles/globals.css";
+import { Nav, Alert } from "components";
 
 export default App;
 
 function App({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Next.js 10 - CRUD Example with React Hook Form</title>
+  return (
+    <>
+      <Head>
+        <title>Next Js Tutorial - Muthu</title>
 
-                {/* bootstrap css */}
-                <link href="//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
-            </Head>
+        {/* bootstrap css */}
+        <link
+          href='//netdna.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+          rel='stylesheet'
+        />
+      </Head>
 
-            <div className="app-container bg-light">
-                <Nav />
-                <Alert />
-                <div className="container pt-4 pb-4">
-                    <Component {...pageProps} />
-                </div>
-            </div>
+      <div className='app-container bg-light'>
+        <Nav />
+        <Alert />
+        <div className='container pt-4 pb-4'>
+          <Component {...pageProps} />
+        </div>
+      </div>
 
-            {/* credits */}
-            <div className="text-center mt-4">
-                <p>
-                    <a href="https://jasonwatmore.com/post/2021/04/20/next-js-10-crud-example-with-react-hook-form" target="_top">Next.js 10 - CRUD Example with React Hook Form</a>
-                </p>
-                <p>
-                    <a href="https://jasonwatmore.com" target="_top">JasonWatmore.com</a>
-                </p>
-            </div>
-        </>
-    );
+      {/* credits */}
+    </>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ export default Home;
 function Home() {
     return (
         <div>
-            <h1>Next.js 10 - CRUD Example with React Hook Form</h1>
+            <h1> Next Js Tutorial</h1>
             <p>An example app showing how to list, add, edit and delete user records with Next.js 10 and the React Hook Form library.</p>
             <p><Link href="/users">&gt;&gt; Manage Users</Link></p>
         </div>
